20be0a036a9fee078417e000be2147522ba71c1f
Ashfaq Salehin <ashfaq.aws@gmail.com>
2023-12-03 01:41:56 +0000

    Interface functions adjusted

--- src/data/get.rs
+++ src/data/get.rs
@@ -1,3 +1,26 @@
 pub fn load_files(start: u32, end: u32) {
 
 }
+
+pub fn get_start_end_time_given_breakdown(
+    start_day: u32,
+    start_hour: u32,
+    start_minute: u32,
+    start_second: u32,
+    end_day: u32,
+    end_hour: u32,
+    end_minute: u32,
+    end_second: u32
+) -> (u32, u32) {
+    let start_time = start_day * 24 * 60 * 60 +
+        start_hour * 60 * 60 +
+        start_minute * 60 +
+        start_second;
+
+    let end_time = end_day * 24 * 60 * 60 +
+        end_hour * 60 * 60 +
+        end_minute * 60 +
+        end_second;
+
+    (start_time, end_time)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,42 @@
 mod data;
 
 use std::env;
+use std::collections::HashMap;
 
 fn main() {
     let command = env::args().nth(1);
+    let args: Vec<String> = env::args().collect();
+    let mut options: HashMap<&str, u32> = HashMap::new();
+    for arg in args.iter().skip(2) {
+        let parts: Vec<&str> = arg
+            .split("=")
+            .collect();
+        if parts.len() != 2 {
+            eprintln!("Wrong option passed {}", arg);
+            continue;
+        }
+
+        let key = parts[0];
+        let maybe_value = parts[1].parse::<u32>();
+        match maybe_value {
+            Ok(value) => {
+                options.insert(key, value);
+            }
+            _ => {
+                eprintln!("Invalid option passed {}", parts[1]);
+                continue;
+            }
+        }
+    }
 
     match command.as_deref() {
         Some("load") => {
-            match (env::args().nth(2), env::args().nth(3)) {
-                (Some(start_str), Some(end_str)) => {
-                    match run_get_data(&start_str, &end_str) {
-                        Ok(_) => {
-                            println!("Downloaded data successfully");
-                        }
-                        Err(msg) => {
-                            eprintln!("Error running downloader {}", msg);
-                        }
-                    };
+            match run_get_data(&options) {
+                Ok(_) => {
+                    println!("Downloaded data successfully");
                 }
-                _ => {
-                    eprintln!("Load requires start and end parameters");
+                Err(msg) => {
+                    eprintln!("Error running downloader {}", msg);
                 }
             }
         }
@@ -32,23 +49,87 @@ fn main() {
     }
 }
 
-fn run_get_data(start_str: &String, end_str: &String) -> Result<(), &'static str> {
-    let maybe_start = start_str.parse::<u32>();
-    let maybe_end = end_str.parse::<u32>();
+fn run_get_data(options: &HashMap<&str, u32>) -> Result<(), &'static str> {
+    let mut start_time: u32 = 0;
+    let mut end_time: u32 = 0;
 
-    return match (maybe_start, maybe_end) {
-        (Ok(start), Ok(end)) => {
-            data::get::load_files(start, end);
-            Ok(())
+    match (options.get("start_time"), options.get("end_time")) {
+        (Some(start), Some(end)) => {
+            start_time = *start;
+            end_time = *end;
         }
-        (Err(_), Ok(_)) => {
-            Err("Invalid start parameter passed")
+        (Some(start), None) => {
+            start_time = *start;
         }
-        (Ok(_), Err(_)) => {
-            Err("Invalid end parameter passed")
+        (None, Some(end)) => {
+            end_time = *end;
         }
-        _ => {
-            Err("Both invalid parameters passed")
-        }
+        _ => {}
+    }
+
+    let start_day = match options.get("start_day") {
+        Some(sd) => { *sd }
+        _ => { 0 }
+    };
+
+    let start_hour = match options.get("start_hour") {
+        Some(sh) => { *sh }
+        _ => { 0 }
+    };
+
+    let start_minute = match options.get("start_minute") {
+        Some(sm) => { *sm }
+        _ => { 0 }
+    };
+
+    let start_second = match options.get("start_second") {
+        Some(ss) => { *ss }
+        _ => { 0 }
+    };
+
+    let end_day = match options.get("end_day") {
+        Some(ed) => { *ed }
+        _ => { 0 }
+    };
+
+    let end_hour = match options.get("end_hour") {
+        Some(eh) => { *eh }
+        _ => { 0 }
+    };
+
+    let end_minute = match options.get("end_minute") {
+        Some(em) => { *em }
+        _ => { 0 }
+    };
+
+    let end_second = match options.get("end_second") {
+        Some(es) => { *es }
+        _ => { 0 }
+    };
+
+    let (calc_start_time, calc_end_time) = data::get::get_start_end_time_given_breakdown(
+        start_day,
+        start_hour,
+        start_minute,
+        start_second,
+        end_day,
+        end_hour,
+        end_minute,
+        end_second
+    );
+
+    if start_time == 0 {
+        start_time = calc_start_time;
+    }
+
+    if end_time == 0 {
+        end_time = calc_end_time;
+    }
+
+    if end_time <= start_time {
+        Err("Invalid start or end parameter passed")
+    } else {
+        data::get::load_files(start_time, end_time);
+        Ok(())
     }
 }
