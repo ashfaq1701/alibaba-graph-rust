6c12637a79a54a25d42df9ff27576f224e04a4af
Ashfaq Salehin <ashfaq.aws@gmail.com>
2023-12-19 03:54:10 +0000

    Plotted data from Rust using python

--- src/lib.rs
+++ src/lib.rs
@@ -13,21 +13,31 @@ mod process;
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 use process::init::run_op_and_return_results;
-use crate::process::structs::WindowResult;
 use crate::utils::env_params::load_env_files;
 
 #[pyfunction]
-pub fn run_op(op_data: Option<&str>) -> PyResult<Vec<WindowResult>> {
-    match run_op_and_return_results(op_data) {
-        Ok(result) => Ok(result),
+pub fn run_op(py: Python, op_data: Option<&str>) -> PyResult<Vec<(f64, u32, u32, u32, u32)>> {
+    py.allow_threads(|| match run_op_and_return_results(op_data) {
+        Ok(result) => {
+            let result_tuples = result
+                .iter()
+                .map(|result| (
+                    result.result,
+                    result.window_idx as u32,
+                    result.stored_window_number,
+                    result.start,
+                    result.end
+                ))
+                .collect();
+            Ok(result_tuples)
+        },
         Err(e) => Err(PyValueError::new_err(format!("{}", e)))
-    }
+    })
 }
 
 #[pymodule]
 fn alibaba_graph_rust(_py: Python, m: &PyModule) -> PyResult<()> {
     pyo3_log::init();
-
     load_env_files();
 
     m.add_function(wrap_pyfunction!(run_op, m)?)?;
