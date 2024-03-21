ccacfc63e4bbf3667a9f1af0e9532048ade5698f
Ashfaq Salehin <ashfaq.aws@gmail.com>
2024-03-21 00:03:56 +0000

    Fix retry logic

--- src/data/download.rs
+++ src/data/download.rs
@@ -9,18 +9,26 @@ pub fn download(url: &str, destination: &str, retries: u32) -> Result<()> {
     info!("Starting download of {} to the {}", url, destination);
 
     for attempt in 0..=retries {
-        let mut response = reqwest::blocking::get(url);
+        let response = reqwest::blocking::get(url);
 
-        if response.is_ok() && response.unwrap().status().is_success() {
-            let mut file = File::create(destination)?;
-            copy(&mut response, &mut file)?;
-            info!("File downloaded successfully to: {}", destination);
-            return Ok(());
-        } else {
-            error!("Download attempt {} failed. Status code: {}", attempt + 1, response.status());
-            let backoff_duration = Duration::from_secs(2u32.pow(attempt) as u64);
-            std::thread::sleep(backoff_duration);
+        match response {
+            Ok(mut resp) => {
+                if resp.status().is_success() {
+                    let mut file = File::create(destination)?;
+                    copy(&mut resp, &mut file)?;
+                    info!("File downloaded successfully to: {}", destination);
+                    return Ok(());
+                } else {
+                    error!("Download attempt {} failed. Status code: {}", attempt + 1, resp.status());
+                }
+            }
+            Err(e) => {
+                error!("Download attempt {} failed. Error: {}", attempt + 1, e)
+            }
         }
+
+        let backoff_duration = Duration::from_secs(2u32.pow(attempt) as u64);
+        std::thread::sleep(backoff_duration);
     }
 
     Err(anyhow!("Failed to download {} after {} retries", url, retries))
